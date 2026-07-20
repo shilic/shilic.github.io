b932f8184a6861b49cfff694e4349d593d0a476f
shilic <985478238@qq.com>
2026-07-20 03:05:29 +0000

    Deploying to gh-pages from @ shilic/shilic.github.io@97d8f047180d18d06b3857a7bcf37b613186862a 🚀

--- assets/architecture-TIHT7OUA-V6N4J3SY-D-4wHL5I.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./chunk-NV3KIAZN-DVhPGIwb.js";export{e as createArchitectureServices};
---
+++ assets/swimlanesDiagram-OX7UBUGT-C20JTl1M.js
@@ -0,0 +1,8 @@
+import{t as e}from"./chunk-AQ6EADP3-CZhslHi-.js";import"./chunk-LIEV3EAG-pyKGssV3.js";import"./chunk-KEUXMURM-DjQCu1kw.js";import"./chunk-MXWVPPQS-BNkKlmGl.js";import"./chunk-STOV2HOB-GnExfzIX.js";import"./chunk-ENMKPL7Y-ByxqN-RQ.js";import"./chunk-SZQNAPRP-D0mnIa0m.js";import"./chunk-AZZRMDJM-y-xJBuMz.js";import"./chunk-5VCL7Z4A-CQ-2dYWj.js";import"./chunk-STADKHD7-C8BOcPre.js";import"./chunk-HCQLZPVX-C9FjtqSm.js";import"./chunk-VE5CLXGZ-C1vB7wpY.js";import"./chunk-4S3VB3X6-9zZLkOeR.js";import"./chunk-5IMINLNL-DOdTeQri.js";import"./chunk-SXFNDEX5-CgMRGTqw.js";import"./chunk-KTKOSTGQ-Bq26T95m.js";import"./chunk-6C3L5S6U-Bf2tY1Dk.js";import"./chunk-QU2NO3GS-DZcAW9Iv.js";import"./chunk-7FYTHRHK-CsmYIswB.js";import{n as t,t as n}from"./chunk-LUNKGL7L-IpckbE1V.js";var r=n({defaultLayout:`swimlane`,styles:e(e=>`${t(e)}
+  .swimlane.cluster rect {
+    stroke: ${e.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,`getStyles`)});export{r as diagram};
